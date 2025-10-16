767cee4392d9f5026aebb0a9c9798d3d1de89a97
safarashlimp <rashlisafa04@gmail.com>
2025-10-16 12:15:17 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/mechine_test_noviindus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mechine_test_noviindus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/login/data/model/login_model.dart
@@ -0,0 +1,21 @@
+// @freezed
+// class Welcome with _$Welcome {
+//     const factory Welcome({
+//         @JsonKey(name: "status")
+//         required bool status,
+//         @JsonKey(name: "message")
+//         required String message,
+//         @JsonKey(name: "validation_errors")
+//         required ValidationErrors validationErrors,
+//     }) = _Welcome;
+// }
+
+// @freezed
+// class ValidationErrors with _$ValidationErrors {
+//     const factory ValidationErrors({
+//         @JsonKey(name: "phone")
+//         required List<String> phone,
+//         @JsonKey(name: "country_code")
+//         required List<String> countryCode,
+//     }) = _ValidationErrors;
+// }
